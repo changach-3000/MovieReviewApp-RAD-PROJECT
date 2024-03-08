f9d034b3bcbcbf8ed407fecb3880a3fbb2df84e9
Chang'ach <126554394+changach-3000@users.noreply.github.com>
2024-03-08 11:20:34 +0300

    update the landing page and editing the signup and login buttons

--- frontend/src/components/Landing.jsx
+++ frontend/src/components/Landing.jsx
@@ -3,11 +3,22 @@ import { NavLink } from 'react-router-dom'
 
 function Landing() {
   return (
-    <div className='landing'>
-        <img src="/images/login-background.jpg" className='landing-img'/>
-       <NavLink to="home"><button className='centered'>GET MORE HERE</button></NavLink> 
-        <p className='centered2'>Get the Best watching experience there is</p>
+<div className="h-screen">
+  <div className="h-full bg-cover w-full" style={{backgroundImage: `url('/images/login-background.jpg')`}}>
+    <div className="flex flex-col justify-center items-center h-full bg-black bg-opacity-50">
+      <p className="text-white text-center text-xl md:text-3xl lg:text-4xl ">Movies, Series and More</p>
+      <p className='text-white text-center fw-lighter md:text-lg px-5 py-3 md:px-10 max-w-md'>We offer world-class movie reviews for the lowest prices and for the best service.</p>
+      <div className="grid grid-cols-2 gap-4 mt-4">
+        <NavLink to="/signup">
+          <button type="button" className="btn border-white border-2 hover:bg-yellow-400 text-white bg-amber-500 px-4">Sign Up</button>
+        </NavLink>
+        <NavLink to="/login">
+          <button type="button" className="btn border-white border-2 hover:bg-yellow-400 text-white bg-amber-500 px-4">Log In</button>
+        </NavLink>
+      </div>
     </div>
+  </div>
+</div>
   )
 }
 
