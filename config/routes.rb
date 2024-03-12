2555157136f2791110a636afda4cd1eb1a46b3b9
Chang'ach <126554394+changach-3000@users.noreply.github.com>
2024-03-12 20:29:41 +0300

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
   Rails.application.routes.draw do
-  
+    root :to => 'movies#index'
     post "/api/auth/login", to:"session#login"
     delete "/api/auth/logout", to:"session#logout"
     get "/api/current_user", to:"users#current_user"
@@ -8,9 +8,6 @@
     resources :reviews, only: [:index, :create, :update, :destroy]
     resources :movies, only: [:index, :create, :destroy, :show]
     resources :users, only: [:index, :show, :create]
-  
-  #   end
-    # get '/api/foods/:id', to: 'foods#single_food', as: 'single_food'
   
    
     # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html 
